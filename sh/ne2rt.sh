05cf4c926e3e1f97a17764f2a0b5f4d35c45d197
Hanxiao Wu <89796187+Hanxiao-Wu@users.noreply.github.com>
2024-05-13 16:47:46 -0400

    Update ne2rt.sh

--- sh/ne2rt.sh
+++ sh/ne2rt.sh
@@ -1,9 +1,9 @@
-dir=/NAS1/Antarctica/Season2023/SOLODATA/SAC/NE_SW/EQ
-for ev in `ls -d *T*Z`
+dir='./EQ'
+for ev in `ls -d 2023*`
 do
 echo $ev
 cd ${dir}/$ev
-ls *.Z.SAC.Pcut_high | awk -F '.' '{print $1}' | sort -u > sta.list
+ls *.Z.SAC.P | awk -F '.' '{print $1}' | sort -u > sta.list
 for st in `awk '{print $1}' sta.list`
 do
 sac<<EOF
