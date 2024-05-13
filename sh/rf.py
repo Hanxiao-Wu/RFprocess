16613b847349ff2ee1f844e4a2287d533601ad33
Hanxiao Wu <89796187+Hanxiao-Wu@users.noreply.github.com>
2024-05-13 16:51:10 -0400

    Add files via upload

---
+++ sh/rf.py
@@ -0,0 +1,29 @@
+from obspy.core.stream import read
+from seispy import decon
+import sys
+
+ev=sys.argv[1]
+R = read(ev+'/*.R.SAC.Pcut_high')
+Z = read(ev+'/*.Z.SAC.Pcut_high')
+
+Gaussian_width = 5 #2.5
+nbumps = 200
+phase_delay = 5.
+min_error = 0.001
+dt = R[0].stats.delta
+rf = R.copy()
+
+i = 0
+for st in R:
+    R_tr = R[i].data
+    Z_tr = Z.select(station=R[i].stats.station)[0].data
+    PRF_R,RMS,it = decon.deconit(R_tr,Z_tr,dt,R_tr.shape[0],phase_delay,Gaussian_width,nbumps,min_error)
+    rf[i].data = PRF_R
+    rf[i].stats.channel = 'PRF_R'
+    rf[i].stats.starttime = rf[i].stats.starttime - phase_delay
+    rf[i].stats.sac.user0 = Gaussian_width
+    rf[i].stats.sac.user1 = (1-RMS[-1])*100
+    rf[i].write(ev+'/'+rf[i].stats.station+'.Prf_high',format='SAC')
+    i = i + 1
+
+print('done:',ev)
